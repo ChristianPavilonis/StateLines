20aefb83fe3108f9143c94e76f9ec411b838bea6
Christian Pavilonis <christianpavilonis@Macbooks-MacBook-Pro.local>
2018-01-23 07:36:56 -0600

    Fixes init scripts for rename

--- init.bat
+++ init.bat
@@ -1,13 +1,13 @@
 @echo off
 
 if ["%~1"]==["json"] (
-    copy /-y resources\Homestead.json Homestead.json
+    copy /-y resources\State.json State.json
 )
 if ["%~1"]==[""] (
-    copy /-y resources\Homestead.yaml Homestead.yaml
+    copy /-y resources\State.yaml State.yaml
 )
 
 copy /-y resources\after.sh after.sh
 copy /-y resources\aliases aliases
 
-echo Homestead initialized!
+echo State initialized!
--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ -n "$1" ]]; then
-    cp -i resources/Homestead.json Homestead.json
+    cp -i resources/State.json State.json
 else
-    cp -i resources/Homestead.yaml Homestead.yaml
+    cp -i resources/State.yaml State.yaml
 fi
 
 cp -i resources/after.sh after.sh
 cp -i resources/aliases aliases
 
-echo "Homestead initialized!"
+echo "StateLines initialized!"
